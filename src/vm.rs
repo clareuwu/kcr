34755a8968e56ce001aa0a12fe00e8ca0814ccc5
Clare <me@clare.fyi>
2023-08-06 15:36:37 -0400

    start adding some tests

--- src/vm.rs
+++ src/vm.rs
@@ -1,5 +1,7 @@
 use crate::bytecode::*;
 
+#[derive(Debug)]
+#[derive(PartialEq, Eq)]
 pub enum InterpretResult {
     InterpretOK,
     InterpretCompileError,
@@ -8,10 +10,35 @@ pub enum InterpretResult {
 
 pub struct VM {
     bc: Bytecode,
+    ip: usize,
 }
 
 impl VM {
     pub fn new(bc: Bytecode) -> Self {
-        VM { bc, }
+        VM { bc, ip: 0}
+    }
+
+    pub fn interpret(&mut self) -> InterpretResult {
+        return InterpretResult::InterpretOK;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_new_vm() {
+        let bc = Bytecode::new();
+        let vm = VM::new(bc);
+        assert!(vm.bc.code.is_empty());
+        assert!(vm.bc.constants.is_empty());
+        assert!(vm.ip == 0);
+    }
+
+    #[test]
+    fn test_vm_interpret() {
+        let bc = Bytecode::new();
+        let mut vm = VM::new(bc);
+        assert_eq!(vm.interpret(), InterpretResult::InterpretOK);
     }
 }
